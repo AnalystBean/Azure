8c9af65cc07a40fe515b8b07d36d9e05427d5c10
Mikerosoftpro <bean.michael2528@gmail.com>
2023-02-23 10:14:51 -0500

    Update Databricks_Blob_Read.py

--- Databricks_Blob_Read.py
+++ Databricks_Blob_Read.py
@@ -1,13 +1,9 @@
 from pyspark.sql import SparkSession
 
-# Set up the Spark session
 spark = SparkSession.builder.appName("").getOrCreate()
 
-# Read the CSV file from Blob Storage
 df = spark.read.format("csv").option("", True).load(" ")
 
-# Perform some data transformations
 df = df.filter("").groupBy("").count()
 
-# Write the results back to Blob Storage
 df.write.format("csv").option("", True).save(" ")
