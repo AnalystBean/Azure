a08c0a92706ebd338b802543b8d1ccfd0ba597eb
Mikerosoftpro <bean.michael2528@gmail.com>
2023-04-25 18:03:17 -0400

    Create NTLM_sync.ps1

---
+++ Tools_Templates_Etc/RBAC/NTLM_sync.ps1
@@ -0,0 +1,11 @@
+$adConnector = "<CASE SENSITIVE AD DS CONNECTOR NAME>"
+$azureadConnector = "<CASE SENSITIVE AZURE AD CONNECTOR NAME>"
+Import-Module -Name "C:\Program Files\Microsoft Azure AD Sync\Bin\ADSync"
+$c = Get-ADSyncConnector -Name $adConnector
+$p = New-Object Microsoft.IdentityManagement.PowerShell.ObjectModel.ConfigurationParameter "Microsoft.Synchronize.ForceFullPasswordSync", String, ConnectorGlobal, $null, $null, $null
+$p.Value = 1
+$c.GlobalParameters.Remove($p.Name)
+$c.GlobalParameters.Add($p)
+$c = Add-ADSyncConnector -Connector $c
+Set-ADSyncAADPasswordSyncConfiguration -SourceConnector $adConnector -TargetConnector $azureadConnector -Enable $false
+Set-ADSyncAADPasswordSyncConfiguration -SourceConnector $adConnector -TargetConnector $azureadConnector -Enable $true
